01378d7e43b196a0e09d2aad033111a4eb468934
christian tshikwata <22nt406@esisalama.org>
2026-05-08 13:27:54 +0200

    feat(i18n): traductions LangueProvider FR/EN/SW

---
+++ android/app/src/main/kotlin/cd/udbl/fsi/confidantsante/MainActivity.kt
@@ -0,0 +1,5 @@
+package cd.udbl.fsi.confidantsante
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import cloud_firestore
+import firebase_core
+import flutter_local_notifications
+import flutter_secure_storage_macos
+import sqflite_darwin
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+}
